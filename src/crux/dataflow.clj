9fb51ae7f778b3559940f6df34f5652b8e14b89d
Håkan Råberg <hakan.raberg@gmail.com>
2019-09-09 16:47:27 +0100

    Ability for naive embedding and launching of the dataflow server out of the jar to tmp if specified. #161.

--- src/crux/dataflow.clj
+++ src/crux/dataflow.clj
@@ -2,11 +2,15 @@
   (:require
    [clojure.spec.alpha :as s]
    [clojure.tools.logging :as log]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
    [clj-3df.core :as df]
    [manifold.stream]
    [crux.api :as api]
-   [crux.codec :as c])
+   [crux.codec :as c]
+   [crux.io :as cio])
   (:import java.io.Closeable
+           java.io.File
            java.nio.ByteBuffer))
 
 ;; TODO: This breaks range queries for longs.
@@ -86,34 +90,66 @@
         (log/debug "3DF Tx:" new-transaction)
         @(df/exec! conn (df/transact db new-transaction))))))
 
-(defrecord CruxDataflowTxListener [conn db crux ^Thread worker-thread]
+(defrecord CruxDataflowTxListener [conn db crux ^Thread worker-thread ^Process server-process]
   Closeable
   (close [_]
     (manifold.stream/close! (:ws conn))
     (doto worker-thread
       (.interrupt)
-      (.join))))
+      (.join))
+    (when server-process
+      (doto server-process
+        (.destroy)
+        (.waitFor)))))
 
 (s/def :crux.dataflow/url string?)
 (s/def :crux.dataflow/schema map?)
 (s/def :crux.dataflow/poll-interval nat-int?)
 (s/def :crux.dataflow/debug-connection? boolean?)
+(s/def :crux.dataflow/embed-server? boolean?)
 
 (s/def :crux.dataflow/tx-listener-options (s/keys :req [:crux.dataflow/url
                                                         :crux.dataflow/schema]
                                                   :opt [:crux.dataflow/poll-interval
-                                                        :crux.dataflow/debug-connection?]))
+                                                        :crux.dataflow/debug-connection?
+                                                        :crux.dataflow/embed-server?]))
+
+(def ^:const declerative-server-resource "declarative-server-v0.2.0-x86_64-unknown-linux-gnu")
+
+(defn- linux? []
+  (str/includes? (str/lower-case (System/getProperty "os.name")) "linux"))
+
+(defn- x86_64? []
+  (let [arch (System/getProperty "os.arch")]
+    (or (= "x86_64" arch) (= "amd64" arch))))
+
+(defn start-dataflow-server []
+  (assert (and (linux?) (x86_64?))
+          "Only x86_64-unknown-linux-gnu supported")
+  (let [server-file (io/file (cio/create-tmpdir "declarative-server")
+                             "declarative-server")]
+    (with-open [in (io/input-stream (io/resource declerative-server-resource))
+                out (io/output-stream server-file)]
+      (io/copy in out))
+    (.setExecutable server-file true)
+    (->> (ProcessBuilder. ^"[Ljava.lang.String;" (into-array [(.getAbsolutePath server-file)]))
+         (.inheritIO)
+         (.start))))
 
 (defn start-dataflow-tx-listener ^java.io.Closeable [crux-node
                                                      {:keys [crux.dataflow/url
                                                              crux.dataflow/schema
                                                              crux.dataflow/poll-interval
-                                                             crux.dataflow/debug-connection?]
+                                                             crux.dataflow/debug-connection?
+                                                             crux.dataflow/embed-server?]
                                                       :or {poll-interval 100
                                                            debug-connection? false}
                                                       :as options}]
   (s/assert :crux.dataflow/tx-listener-options options)
-  (let [conn ((if debug-connection?
+
+  (let [server-process (when embed-server?
+                         (start-dataflow-server))
+        conn ((if debug-connection?
                 df/create-debug-conn!
                 df/create-conn!) url)
         db (df/create-db schema)]
@@ -134,4 +170,4 @@
                                              (catch InterruptedException ignore))))
                           (.setName "crux.dataflow.worker-thread")
                           (.start))]
-      (->CruxDataflowTxListener conn db crux-node worker-thread))))
+      (->CruxDataflowTxListener conn db crux-node worker-thread server-process))))
